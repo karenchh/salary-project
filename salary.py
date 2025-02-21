68bd747f3adc6e7ae3c30711508a4264a480cc25
Karen Chahrour <karenchahrour123@gmail.com>
2025-02-21 09:07:46 -0800

    Inputing the percentages

--- salary.py
+++ salary.py
@@ -4,3 +4,6 @@ action = "yes"
 while action == "yes":
     salary = float(input("Please enter your salary: "))
     newMonth = input("Enter the month you are storing the salary for: ")
+    savingspercentage = float(input("Enter the percentage of savings: "))
+    rentpercentage = float(input("Enter the percentage of rent: "))
+    electricitypercentage = float(input("Enter the percentage of electricity: "))
