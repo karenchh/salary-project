97ebb4171a6788bac9310629a59624f6fcf8976d
Karen Chahrour <karenchahrour123@gmail.com>
2025-02-21 09:10:57 -0800

    Calculating the rent,electricity and total

--- salary.py
+++ salary.py
@@ -9,3 +9,9 @@ while action == "yes":
     electricitypercentage = float(input("Enter the percentage of electricity: "))
 
     savings = salary * savingspercentage / 100.0
+    rent = salary * rentpercentage / 100.0
+    electricity = salary * electricitypercentage / 100.0
+    total = savings + rent + electricity
+    remainingsalary = salary - total
+    estimatedyearlycost = (rent + electricity) * 12
+    totalsalarysquared = salary ** 2
